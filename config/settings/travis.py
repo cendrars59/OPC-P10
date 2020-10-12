a4c3b40c7681349b54931f9b782463b5d9f5cf6a
cendrars59 <cyr.leroux.dev@moinslequart.net>
2020-10-12 13:36:46 +0200

    Configure CI to use travis config file

---
+++ config/settings/travis.py
@@ -0,0 +1,14 @@
+from . import *
+
+DATABASES = {
+    'default': {
+        # Using postgresql connector
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pure',  # Database name
+        'USER': 'dick',  # Database user name
+        'PASSWORD': 'rivers',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'TEST': {'NAME': 'jojo', },
+    },
+}
