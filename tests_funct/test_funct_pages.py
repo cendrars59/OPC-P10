bed44c8fb9fa368c9e2984b5555ee667d70f5889
cendrars59 <cyr.leroux.dev@moinslequart.net>
2020-10-12 21:26:49 +0200

    Managing selenium test on travis

--- tests_funct/test_funct_pages.py
+++ tests_funct/test_funct_pages.py
@@ -5,9 +5,10 @@ from django.urls import reverse
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-chrome_options = Options()
+chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
 
