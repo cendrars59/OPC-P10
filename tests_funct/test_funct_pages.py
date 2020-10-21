caf928dca477aabb4d5223143ec0a1b2a3de5f22
cendrars59 <cyr.leroux.dev@moinslequart.net>
2020-10-21 23:26:12 +0200

    Nothing to do

--- tests_funct/test_funct_pages.py
+++ tests_funct/test_funct_pages.py
@@ -3,11 +3,15 @@ import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
 from selenium import webdriver
-#from selenium.webdriver.chrome.options import Options
+
+
+
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('window-size=1920x1080')
+
 
 
 
