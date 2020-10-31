f1221a36c7eac4fc76a88d741db19b14e898683c
cendrars59 <cyr.leroux.dev@moinslequart.net>
2020-10-31 22:00:00 +0100

    Final commit2

--- tests_funct/test_funct_users.py
+++ tests_funct/test_funct_users.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 from selenium import webdriver
 
 chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-gpu')
 # chrome_options.add_argument('--disable-dev-shm-usage')
